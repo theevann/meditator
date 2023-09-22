f36e6af671cf3704a886c107bf40a3708f7919c0
Evann Courdier <evann.courdier@epfl.ch>
2023-09-22 02:14:48 +0200

    Add more languages

--- meditator.py
+++ meditator.py
@@ -10,7 +10,7 @@ from langchain.schema import HumanMessage, SystemMessage
 from voice_synthesis import synthesize_ssml, get_voice_list
 
 
-TESTING = False
+TESTING = True
 
 
 st.set_page_config(
@@ -19,23 +19,33 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded",
     menu_items={
-        'About': "This the meditator app."
+        'Report a bug': 'https://github.com/theevann/meditator/issues',
+        'About': "https://github.com/theevann/meditator/"
     }
 )
 
 languages = {
-    "en-US": "English",
-    "fr-FR": "French"
+    "en-US": "English (US)",
+    "en-GB": "English (GB)",
+    "fr-FR": "French",
+    "es-ES": "Spanish",
+    "it-IT": "Italian",
+    "de-DE": "German",
+    "ru-RU": "Russian",
 }
 
 best_models = {
     "MALE": {
         "en-US": "en-US-Neural2-D",
+        "en-GB": "en-GB-Neural2-D",
         "fr-FR": "fr-FR-Neural2-D",
+        "ru-RU": "ru-RU-Wavenet-D",
     },
     "FEMALE": {
         "en-US": "en-US-Neural2-C",
+        "en-GB": "en-GB-Neural2-C",
         "fr-FR": "fr-FR-Neural2-C",
+        "ru-RU": "ru-RU-Wavenet-C",
     }
 }
 
@@ -75,6 +85,7 @@ def get_voices(locale, gender):
         gender_index = 1 + (gender == "FEMALE")
         voices = [voice for voice in voices if voice.ssml_gender == gender_index]
     return voices
+
 
 def generate_response(input_text, time, max_tokens):
     llm = ChatOpenAI(temperature=1, openai_api_key=st.secrets["OPENAI_API_KEY"], model=openai_model, max_tokens=max_tokens)
@@ -147,6 +158,7 @@ def concatenate_audio(audio1, audio2, pause_duration=0):
     
     return output_audio_io.getvalue()
 
+
 def generate_voice(meditation, music):
     full_audio = None
     with st.status("Generating voice...", expanded=True) as status:
@@ -177,7 +189,7 @@ locale = st.sidebar.selectbox("Select language:", languages.keys(), format_func=
 gender = st.sidebar.radio("Select voice gender:", ["MALE", "FEMALE"])
 
 voice_models = get_voices(locale, gender)
-index = next((i for i, model in enumerate(voice_models) if model.name == best_models[gender][locale]), 0)
+index = next((i for i, model in enumerate(voice_models) if model.name == best_models[gender].get(locale, "")), 0)
 
 if TESTING:
     voice_model = st.sidebar.selectbox("Select voice model:", voice_models, format_func=lambda voice: f"{voice.name}", index=index)
@@ -189,7 +201,7 @@ else:
 time = st.sidebar.slider("Wanted duration (in minutes):", 1, 15, 5, 1)
 
 with st.form("my_form"):
-    text = st.text_input("Optional meditation theme:", max_chars=100, placeholder="What do you want this meditation to be about ?")
+    text = st.text_input("Optional meditation theme:", max_chars=100, placeholder="What do you want this meditation to be about ?", help="You can write for instance: Relaxation, Sleep, Peace, Joy, Sounds, Inner Child, etc.")
     music = st.selectbox("Select music:", list(musics.keys()), format_func=lambda music: f"{music}")
     clicked = st.form_submit_button("Generate !")
 
